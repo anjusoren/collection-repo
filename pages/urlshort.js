b115991f5b65d27452665c82cc18589bedb9e1c8
anjusoren <101965769+anjusoren@users.noreply.github.com>
2022-07-23 20:28:30 +0530

    images added

--- pages/urlshort.js
+++ pages/urlshort.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import Image from 'next/image';
+
 
 function urlshort() {
   return (
-    <div>
-      <h1>Url Shortening Page</h1>
+    <div className="">
+    <div className="Header">
+      <nav className="nav__left">
+      <Image src="https://raw.githubusercontent.com/anjusoren/collection-repo/main/images/bg-intro-desktop.png" alt="shortly logo" width ={200} height={100}/>
+        <ul>
+          <li>Features</li>
+          <li>Pricing</li>
+          <li>Resources</li>
+        </ul>
+      </nav>
+    </div>
+     
     </div>
   )
 }
